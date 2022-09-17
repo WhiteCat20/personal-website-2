087a870636a8844fcc9c0adec9abc057bc4d4885
WhiteCat20 <fzrahmadan@gmail.com>
2022-09-17 10:51:46 +0700

    changed to link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <div className="Navbar px-4 py-10">
         <div className="flex gap-1 justify-center text-2xl text-white">
-          <a
-            href="/"
+          <Link
+            to="/"
             className="inline-block hover:bg-blue-500 hover:cursor-pointer px-3 py-1 pb-2 rounded-xl"
           >
             Intro
-          </a>
-          <a
-            href="/profile"
+          </Link>
+          <Link
+            to="/profile"
             className="inline-block hover:bg-blue-500 hover:cursor-pointer px-3 py-1 pb-2 rounded-xl"
           >
             Profile
-          </a>
-          <a
-            href="/project"
+          </Link>
+          <Link
+            to="/project"
             className="inline-block hover:bg-blue-500 hover:cursor-pointer px-3 py-1 pb-2 rounded-xl"
           >
             Projects
-          </a>
+          </Link>
         </div>
       </div>
     </>
